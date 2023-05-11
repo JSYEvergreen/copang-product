b792c7f5e695b7831b2d4294d5372acd6ecc5e5a
PunRabbit <tjdmsch2201@gmail.com>
2023-05-12 01:04:42 +0900

    DB Core Build

--- infra/postgresql/core.py
+++ infra/postgresql/core.py
@@ -1,6 +1,21 @@
+import tomlkit
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Engine, Connection
+
+from infra.postgresql.postgresql import DBConfig
 
 
 class PostGreSQLCore:
     def __init__(self):
-        pass
+        with open("core/Postgresql.toml", "r") as toml_file:
+            self.config: DBConfig = DBConfig(**tomlkit.load(toml_file))
+        self.connection_string: str = f"{self.config.name}://{self.config.user}:{self.config.password}@{self.config.host}:{self.config.port}/{self.config.database}"
+        self.engine: Engine = create_engine(self.connection_string, pool_recycle=500)
+
+    def create_session(self):
+        session = sessionmaker(bind=self.engine)
+        return session()
+
+    def create_connection(self) -> Connection:
+        return self.engine.connect()
 
---
+++ infra/postgresql/postgresql.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class DBConfig(BaseModel):
+    name: str
+    user: str
+    password: str
+    host: str
+    port: int
+    database: str
+
+
